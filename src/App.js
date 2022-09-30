6dafc57ee79db4e9e31a473b74723d84266b0ce9
Kevin La <chenchiaho@gmail.com>
2022-09-30 13:53:25 +0700

    set up navigation

--- src/App.js
+++ src/App.js
@@ -1,60 +1,23 @@
-import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => {
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
-  
-
-  return (
-    <div className='categories-container'>
-      {categories.map(( category ) => (
-          <CategoryItem key={category.id} category={category} />
-          
-
-
-        // <div  key={id} className='category-container'>
-        //   <div className='background-image' style={{
-        //     backgroundImage: `url(${imageUrl})`
-        //   }}/>
-        //   <div className='category-body-container'>
-        //     <h2>{title}</h2>
-        //     <p>Shop Now</p>
-        //   </div>
-        // </div>
-      ))}
-    </div>
-  );
-
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
 };
 
-
-
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
