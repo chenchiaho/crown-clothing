c55de6b714c61736e9a07b38a13414fbde3bee4f
Kevin La <chenchiaho@gmail.com>
2022-11-07 00:36:46 +0800

    initializing product list

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
     getAuth,
     signInWithRedirect,
@@ -7,9 +7,9 @@ import {
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
-} from 'firebase/auth';
-
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+    signOut,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCucJBA8V5uUoj-vEeml29W9ZcYY9rsmm0",
@@ -26,7 +26,7 @@ const firebaseApp = initializeApp(firebaseConfig);
 const googleProvider = new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
-    prompt: 'select_account',
+    prompt: "select_account",
 });
 
 export const auth = getAuth();
@@ -43,7 +43,7 @@ export const createUserDocumentFromAuth = async (
 ) => {
     if (!userAuth) return;
 
-    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userDocRef = doc(db, "users", userAuth.uid);
 
     const userSnapshot = await getDoc(userDocRef);
 
@@ -59,7 +59,7 @@ export const createUserDocumentFromAuth = async (
                 ...additionalInformation,
             });
         } catch (error) {
-            console.log('error creating the user', error.message);
+            console.log("error creating the user", error.message);
         }
     }
 
@@ -77,3 +77,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
     return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => await signOut(auth);
